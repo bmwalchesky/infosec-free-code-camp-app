00346882127fbd0b4b3bf6361ad7bdd6faff5af4
Bri <briwalchesky@Bris-MacBook-Pro.local>
2025-05-05 21:56:28 -0400

    Hide Powered By

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-app.use(helmet.hidePoweredBy());
+
+helmet.hidePoweredBy()
+
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
